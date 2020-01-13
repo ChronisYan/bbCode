d25edd63d111992f8e55473a1e95f78705ef2445
Chronis <thegamedevelopergame@gmail.com>
2020-01-13 04:13:16 +0200

    418 path!

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
 	res.send('Welcome to bbCode my  Dudes');
 });
 
+app.get('/supersecret', (req, res) => {
+	res.status(418).set('-x', 'Tea is good!').send("418 I'm a Teapot! Congrats you found the Super Sercet Path");
+});
+
 app.listen(port, () => {
 	console.log(`server is up listening on port ${port}!`);
 });
